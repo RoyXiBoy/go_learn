3d2e4f2ae60a86b727cf902105ae1ae0eb8ea036
xlt <aa543187001@163.com>
2018-04-04 18:13:20 +0800

    leetcode 第五題 回文串 概念搞错了

---
+++ learn/leecode/longestPalindrome.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+//完全理解理解错误，回文串是 对称的，abcba这种
+func main(){
+	s := "abcda"
+	fmt.Println(longestPalindrome(s))
+}
+
+func longestPalindrome(s string) string {
+
+
+	return strings.Join(result,"")
+
+}
+
+
